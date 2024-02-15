b84b706be830f67d7ffbc1c351e184e4d36d107d
Ziad Wael <76755899+ziaadwael@users.noreply.github.com>
2024-02-16 00:29:49 +0200

    Elzero Template Three
    Elzero Template Three Using HTML,CSS Only For Practice the CSS after finished Course ^_^

---
+++ Elzero-Template Three/main.js
@@ -0,0 +1,12 @@
+// Just A Little Codes ^_^
+
+
+function OpenNavigationonClick() {
+    document.getElementById("mega_menu").style.display = "block";
+    document.getElementById("mega_menu").style.width = "70%";
+
+}
+
+function CloseNavigationOnLeave() {
+    document.getElementById("mega_menu").style.display = "none";
+}
